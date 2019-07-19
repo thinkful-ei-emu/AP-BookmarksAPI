64ff8e093225de3a1797e93424fedd496dd4b553
Arpan Patel <adp8ke@gmail.com>
2019-07-19 15:29:00 -0400

    added testing for post

--- test/app.spec.js
+++ test/app.spec.js
@@ -26,3 +26,121 @@ describe('GET /bookmarks/:id', () => {
       .expect(404, '404 Not Found')
   })
 })
+
+
+describe('POST /bookmarks', () => {
+  it('POST /bookmarks works correctly', () => {
+    const dummy = {
+        "title": "Google",
+        "url": "https://www.google.com",
+        "description": "Search some things",
+        "rating": "5"
+    }
+
+    return supertest(app)
+      .post('/bookmarks')
+      .send(dummy)
+      .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+      .expect(201)
+      .then(res=>{
+        expect(res.body.title).to.eql(dummy.title)
+        expect(res.body.url).to.eql(dummy.url)
+        expect(res.body.description).to.eql(dummy.description)
+        expect(res.body.rating).to.eql(dummy.rating)
+      })
+  })
+
+  it('Get 400 if no title', () => {
+
+    const dummy = {
+      "url": "https://www.google.com",
+      "description": "Search some things",
+      "rating": "5"
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a title')
+  })
+
+  it('Get 400 if no url', () => {
+
+    const dummy = {
+      "title": 'Google',
+      "description": "Search some things",
+      "rating": "5"
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a valid url')
+  })
+
+  it('Get 400 if not valid url', () => {
+
+    const dummy = {
+      "title": 'Google',
+      "url": "dummy",
+      "description": "Search some things",
+      "rating": "5"
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a valid url')
+  })
+
+  it('Get 400 if no description', () => {
+
+    const dummy = {
+      "title": "Google",
+        "url": "https://www.google.com",
+        "rating": "5"
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a description')
+  })
+
+  it('Get 400 if no rating', () => {
+
+    const dummy = {
+      "title": "Google",
+      "url": "https://www.google.com",
+      "description": "Search some things"
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a valid rating')
+  })
+
+  it('Get 400 if rating is greater than 5 or less than 0', () => {
+
+    const dummy = {
+      "title": "Google",
+      "url": "https://www.google.com",
+      "description": "Search some things",
+      // "rating": 14,
+      "rating": -1
+    }
+
+    return supertest(app)
+    .post('/bookmarks')
+    .send(dummy)
+    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+    .expect(400, 'Enter a valid rating')
+  })
+
+})
