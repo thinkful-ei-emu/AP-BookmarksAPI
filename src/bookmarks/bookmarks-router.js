d9f6fa007eac78db042d54f6aa74d73adaa6714d
Arpan Patel <adp8ke@gmail.com>
2019-07-19 15:12:45 -0400

    post working through postman and validations are working

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const bookmarksRouter = express.Router();
 const bookmarks = require("../store");
-
+const uuid = require('uuid/v4')
+const bodyParser = express.json()
+const validUrl = require('valid-url'); 
 
 const app = express();
 
-bookmarksRouter.route("/bookmarks").get((req, res) => {
+bookmarksRouter
+.route("/bookmarks")
+.get((req, res) => {
   res.json(bookmarks);
-});
+})
+.post(bodyParser, (req, res)=>{
+    const {title, url, description, rating} = req.body;
+
+
+    if(!title){
+        return res.status(400).send('Enter a title')
+    }
+
+    if(!url || !validUrl.isUri(url)){
+        return res.status(400).send('Enter a valid url')
+    }
+
+    if(!description){
+        return res.status(400).send('Enter a description')
+    }
+
+    if(!rating || rating < 0 || rating > 5){
+        return res.status(400).send('Enter a valid rating')
+    }
+
+    const bookmark = {
+        id: uuid(),
+        title,
+        url,
+        description,
+        rating
+        }
+
+    bookmarks.push(bookmark)
+
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark)
+})
 
 bookmarksRouter.route("/bookmarks/:id").get((req, res) => {
     const { id } = req.params;
